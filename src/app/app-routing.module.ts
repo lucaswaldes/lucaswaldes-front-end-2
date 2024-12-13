d8861335e30b1dbca00fb84ced1ab96379ecd68a
lucaswaldes <waldeslucas@gmail.com>
2024-12-12 21:29:30 -0300

    feat: upload project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '',
+		component: HomeComponent,
+		pathMatch: 'full',
+	},
+    
+	{
+		path: '**',
+		redirectTo: '/404',
+		data: { title: 'Página não encontrada' },
+	},
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
